d727253b096e67db6118de5c1ee9eea3caf6cec5
Daniel <daniel.rasmussen@drgeo.se>
2021-06-05 19:51:46 +0200

    updates

--- assets/js/drgeo/conf/conf.js
+++ assets/js/drgeo/conf/conf.js
@@ -2,12 +2,31 @@ const name = 'CONFIG QUIZ';
 const prefix = '../../../images/quiz/';
 const wrongName = 'wrong-';
 
+const cls = {
+    btns: {
+        play: 'submit-button'
+    }
+}
 
+const ids = {
+    score: {
+        id: 'score'
+    },
+    clock: {
+        id: 'clock'
+    },
+    staff: {
+        id: 'staff'
+    }
+
+}
 const assets = {
     prefix: prefix + '/'
 }
 export default { 
     name,
     assets,
-    wrongName
+    wrongName,
+    cls,
+    ids
 }
